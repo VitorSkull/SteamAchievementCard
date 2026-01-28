e887086a606ecd759fc683849117c956c12337b4
Vitor Assis <vitorskull46@gmail.com>
2026-01-28 15:36:28 -0300

    created gitignore for node_modules

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,21 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div class="Menu">
-    <img src="../img/Steam_icon_logo.svg.png" class="SteamLogo">
+    <img src="../img/Steam_icon_logo.svg.png">
     <h1 class="Title">Completed Steam games card creator</h1>
     <p class="subTitle">Custom made cards for your completed games!</p>
-  </div>
-  <div>
     <input type="Title" class="Search" placeholder="Enter your Steam profile URL or custom ID">
+    <button>Check your cards</button>
   </div>
-  <button>Check your cards</button>
-  <p>TESTE</p>
 </template>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&family=Chau+Philomene+One:ital@0;1&display=swap');
 
-.SteamLogo{
+/* .SteamLogo{
   height: 8%;
   width: 8%;
-}
+} */
 .Title{
   font-family: 'Assistant', sans-serif;
   width: 40%;
@@ -40,13 +37,34 @@ import HelloWorld from './components/HelloWorld.vue'
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  & img{
+  height: 8%;
+  width: 8%;
+  }
+  & button{
+    width: 18%;
+    height: 40px;
+    border-radius: 5px;
+    background-color: #00a2ff;
+    border: none;
+    margin-top: 10px;
+    box-shadow: inset 0px 0px 5px rgba(0, 0, 0, 0.5);
+  }
 }
 .Search{
   border-radius: 8px;
   background-color: white;
   font-size: large;
   width: 35%;
+  height: 25px;
   color: black;
+
+  padding-left: 40px;
+
+  background-image: url('/img/loupe.png');
+  background-repeat: no-repeat;
+  background-position: 10px center;
+  background-size: 18px;
 
 }
 .Search::placeholder{
@@ -55,8 +73,5 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .teste{
   background-color: red;
-}
-.SearchButtom{
-  
 }
 </style>
