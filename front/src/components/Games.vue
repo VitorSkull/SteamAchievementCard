7fb019f1013ee2de8b88929342c8f0d8e844b7c2
VitorSkull <vitorskull46@gmail.com>
2026-03-06 09:59:15 -0300

    Games.vue componente refactoring

--- front/src/components/Games.vue
+++ front/src/components/Games.vue
@@ -1,17 +1,25 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, reactive } from 'vue';
 import { useUserStore } from '../stores/userStore';
 import axios from 'axios';
 
 const user = useUserStore()
-console.log(user.steamInput)
-onMounted (async () => {
+const userData = reactive({
+    steamid: '',
+    personaname: '',
+    profileurl: '',
+    avatar: '',
+})
+const fetchUser = async () => {
     const res = await axios.get('http://localhost:8080/Games', {
         params: {steamid: user.steamInput}
     })
-    user.data = res.data
-    console.log(user.data)
-})
+    const data = res.data
+    Object.assign(userData, res.data)
+    console.log(userData)
+}
+console.log(user.steamInput)
+onMounted(fetchUser)
 </script>
 
 <template>
